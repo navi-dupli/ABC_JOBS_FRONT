52b8476afd9fcfb447d08a620d29b97e8c43f48a
nataliesantiagou <n.santiago@uniandes.edu.co>
2023-10-12 01:09:41 -0500

    Se ajustan archivos environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,7 @@
 
 export const environment = {
   production: false,
-  grant_type: "password",
-  username: "mi13dj22@gmail.com",
-  password: "prueba$123",
-  audience: "http://abc-jobs.users-api.dev", 
-  client_id: "pWsYKGDM60cdtDOwAdGZyT9ipiAcHR4N",
-  client_secret: "XU_cTmnfLTog_Nc9whCHTtz1bTTkhT6vvT-4uruXavU7546z5XxERkaRoRZUH2Zw",
-  url_login: "https://abc-jobs-dev.us.auth0.com/oauth/token"
+  
 };
 
 /*
