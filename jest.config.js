db86c01ab09fd1915622c2d536a39568f720a45b
nataliesantiagou <n.santiago@uniandes.edu.co>
2023-10-13 01:14:17 -0500

    Configuracion cobertura codigo, llamado selector idioma

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,14 @@ module.exports = {
             stringifyContentPathRegex: '\\.(html|svg)$',
         },
     },
+    coverageThreshold: {
+        "global": {
+            "branches": 70,
+            "functions": 70,
+            "lines": 70,
+            "statements": 70
+        }
+    },
     coverageDirectory: './coverage',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
