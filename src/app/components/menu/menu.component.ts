f341d2a1cda920b557e0ea5f0c13a93832342ccc
plinio <p.grijalbar@uniandes.edu.co>
2023-10-18 13:50:56 -0500

    Filter menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as jose from 'jose';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,30 +7,56 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
-  model: any[] = [];
+  model: any[] = [
+    {
+      label: 'Home',
+      items: [
+        { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'], scope: ['read:users'] }
+      ]
+    },
+    {
+      label: 'Crear proyecto',
+      items: [
+        { label: 'Crear proyecto', icon: 'pi pi-fw pi-id-card', routerLink: ['/crear-proyecto'], scope: ['register:project'] }
+      ]
+    },
+    {
+      label: 'Registrar empresa',
+      items: [
+        { label: 'Registrar empresa', icon: 'pi pi-fw pi-id-card', routerLink: ['/registrar-empresa'], scope: ['register:company'] }
+      ]
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
-    this.model = [
-      {
-        label: 'Home',
-        items: [
-          { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
-        ]
-      },
-      {
-        label: 'Crear proyecto',
-        items: [
-          { label: 'Crear proyecto', icon: 'pi pi-fw pi-id-card', routerLink: ['/crear-proyecto'] }
-        ]
-      },
-      {
-        label: 'Registrar empresa',
-        items: [
-          { label: 'Registrar empresa', icon: 'pi pi-fw pi-id-card', routerLink: ['/registrar-empresa'] }
-        ]
+    this.filterMenu();
+  }
+
+  getScopes(){
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const decodeToken = jose.decodeJwt(currentUser.access_token);
+    return decodeToken["permissions"] as string[];
+  }
+
+  filterMenu() {
+    const permissions = this.getScopes();
+    this.model = this.model.map((item) => {
+      if (item.items) {
+        item.items = item.items.map((subItem) => {
+           subItem.scope = subItem.scope.filter((scope) => {
+            return permissions.includes(scope);
+          });
+          return subItem.scope.length > 0 ? subItem : null;
+        });
+        item.items = item.items.filter((subItem) => {return subItem !== null});
+        return item;
+      } else {
+       return item.scope.find((scope) => {
+         return permissions.includes(scope);
+       }) ? item : null;
       }
-    ];
+    });
   }
 }
