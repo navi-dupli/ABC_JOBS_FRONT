720b760d1b9fc984b7d4effaf466f03cb5c4b3e0
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2023-10-22 09:03:29 -0500

    cambio del endpoint de login

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -15,15 +15,10 @@ export class AuthService {
   login(username: string, password: string) {
     const headers = this.headers;
     const body = {
-      grant_type: environment.grant_type,
-      username,
+      email: username,
       password,
-      audience: environment.audience,
-      client_id: environment.client_id,
-      client_secret: environment.client_secret,
-      scope: 'openid profile email'
     };
-    return this.http.post<any>(`${environment.url_auth}/oauth/token`, body, { headers });
+    return this.http.post<any>(`${environment.url_api}/login`, body, { headers });
   }
 
   logout() {
