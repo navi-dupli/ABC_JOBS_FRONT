81c3591bbe1a060617acdbbf44a6917a62e8ba51
nataliesantiagou <n.santiago@uniandes.edu.co>
2023-11-16 22:45:04 -0500

    Se ajusta el envio del id del usuairo en el detalle de la cita

--- src/app/appointments/interview-detail/interview-detail.component.ts
+++ src/app/appointments/interview-detail/interview-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {DetailInterviewService} from "../../services/detail-interview/detail-interview.service";
 import {ActivatedRoute} from "@angular/router";
 import {DetailInterviewModel} from "../../models/detail-interview";
+import { SessionService } from 'src/app/services/auth/session.service';
 
 @Component({
   selector: 'app-interview-detail',
@@ -12,15 +13,18 @@ export class InterviewDetailComponent {
 
   detailInterview: DetailInterviewModel;
   showError = false;
+  user: any;
 
   constructor(private detailInterviewService: DetailInterviewService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private sessionService: SessionService) {
+    this.user = this.sessionService.getUser();
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const idInterview = params['id'];
-      this.detailInterviewService.getDetailInterview(idInterview).subscribe(
+      this.detailInterviewService.getDetailInterview(idInterview, this.user.id).subscribe(
         {
           next: (response: any) => {
             this.detailInterview = response;
