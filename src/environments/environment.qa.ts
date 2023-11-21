19292a5985a8ed1ed2cd7ba1fd67391896c5e211
plinio <p.grijalbar@uniandes.edu.co>
2023-11-20 21:52:11 -0500

    Prueba build prod

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  grant_type: "password",
+  username: "mi13dj22@gmail.com",
+  password: "prueba$123",
+  audience: "http://abc-jobs.users-api.dev", 
+  client_id: "pWsYKGDM60cdtDOwAdGZyT9ipiAcHR4N",
+  client_secret: "XU_cTmnfLTog_Nc9whCHTtz1bTTkhT6vvT-4uruXavU7546z5XxERkaRoRZUH2Zw",
+  url_auth: "https://abc-jobs-dev.us.auth0.com",
+  url_api: "https://api-gateway-44q73j2oha-uc.a.run.app"
+};
