617996a449b8ec46b5f34ce96c7f2f8b97531fbc
mviviana <m.ojedan@uniandes.edu.co>
2023-11-24 11:16:56 -0500

    Feature/abcj 53 actualizar perfil (#74)
    * se agrega componente
    
    * update gets
    
    * se llama metodos
    
    * update package
    
    * update nil value

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,10 +6,13 @@ import { CreateProjectComponent } from '../projects/create-project/create-projec
 import { AddCandidateTeamComponent } from '../teams/add-candidate-team/add-candidate-team.component';
 import { HomeComponent } from './home.component';
 import { RegisterTechnicalTestComponent } from '../test/register-technical-test/register-technical-test.component';
-import {InterviewDetailComponent} from "../appointments/interview-detail/interview-detail.component";
+import { InterviewDetailComponent } from "../appointments/interview-detail/interview-detail.component";
 import { ListAppointmentsComponent } from '../appointments/list-appointments/list-appointments.component';
 import { PerformanceEvaluationComponent } from '../test/performance-evaluation/performance-evaluation.component';
 import { CreateTeamComponent } from '../teams/create-team/create-team.component';
+import { UpdateProfileComponent } from '../profile/update-profile/update-profile.component';
+import { UpdateEducationComponent } from '../profile/update-education/update-education.component';
+import { WorkExperienceComponent } from '../profile/work-experience/work-experience.component';
 
 @NgModule({
 	imports: [RouterModule.forChild([
@@ -24,7 +27,10 @@ import { CreateTeamComponent } from '../teams/create-team/create-team.component'
 				{ path: 'detalle-entrevista/:id', component: InterviewDetailComponent },
 				{ path: 'listar-citas', component: ListAppointmentsComponent },
 				{ path: 'evaluar-desempeño', component: PerformanceEvaluationComponent },
-				{ path: 'crear-equipo', component: CreateTeamComponent }
+				{ path: 'crear-equipo', component: CreateTeamComponent },
+				{ path: 'completar-perfil', component: UpdateProfileComponent },
+				{ path: 'actualizar-educacion', component: UpdateEducationComponent },
+				{ path: 'actualizar-experiencia-laboral', component: WorkExperienceComponent },
 			]
 		},
 	])],
